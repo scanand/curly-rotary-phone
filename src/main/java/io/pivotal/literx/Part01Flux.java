051e075da9103095d16ba7ebd0b7b56d2be3a1d0
Anand Pardhi <mail@anand-pardhi.com>
2021-09-23 18:20:35 +1000

    first commit

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -2,6 +2,9 @@ package io.pivotal.literx;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.util.Arrays;
+
 /**
  * Learn how to create Flux instances.
  *
@@ -11,38 +14,40 @@ import reactor.core.publisher.Flux;
 public class Part01Flux {
 
 //========================================================================================
-
-	// TODO Return an empty Flux
-	Flux<String> emptyFlux() {
-		return null;
-	}
-
-//========================================================================================
-
-	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
-	Flux<String> fooBarFluxFromValues() {
-		return null;
-	}
+    
+    // TODO Return an empty Flux
+    Flux<String> emptyFlux() {
+        return Flux.empty();
+    }
 
 //========================================================================================
-
-	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
-	Flux<String> fooBarFluxFromList() {
-		return null;
-	}
-
-//========================================================================================
-
-	// TODO Create a Flux that emits an IllegalStateException
-	Flux<String> errorFlux() {
-		return null;
-	}
+    
+    // TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+    Flux<String> fooBarFluxFromValues() {
+        return Flux.just("foo",
+                         "bar");
+    }
 
 //========================================================================================
+    
+    // TODO Create a Flux from a List that contains 2 values "foo" and "bar"
+    Flux<String> fooBarFluxFromList() {
+        return Flux.fromIterable(Arrays.asList("foo",
+                                               "bar"));
+    }
 
-		// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
-	Flux<Long> counter() {
-		return null;
-	}
+//========================================================================================
+    
+    // TODO Create a Flux that emits an IllegalStateException
+    Flux<String> errorFlux() {
+        return Flux.error(new IllegalStateException());
+    }
 
+//========================================================================================
+    
+    // TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+    Flux<Long> counter() {
+        return Flux.interval(Duration.ofMillis(1)).take(10);
+    }
+    
 }
--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -16,58 +16,90 @@
 
 package io.pivotal.literx;
 
-import java.util.function.Supplier;
-
 import io.pivotal.literx.domain.User;
+import org.assertj.core.api.Assertions;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.function.Supplier;
 
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
  *
  * @author Sebastien Deleuze
- * @see <a href="https://projectreactor.io/docs/test/release/api/reactor/test/StepVerifier.html">StepVerifier Javadoc</a>
+ * @see <a href="https://projectreactor.io/docs/test/release/api/reactor/test/StepVerifier.html">StepVerifier
+ * Javadoc</a>
  */
 public class Part03StepVerifier {
 
 //========================================================================================
-
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
-	void expectFooBarComplete(Flux<String> flux) {
-		fail();
-	}
-
-//========================================================================================
-
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
-	void expectFooBarError(Flux<String> flux) {
-		fail();
-	}
+    
+    // TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes
+    //  successfully.
+    void expectFooBarComplete(Flux<String> flux) {
+        StepVerifier.create(flux)
+                .expectNext("foo",
+                            "bar")
+                .verifyComplete();
+    }
 
 //========================================================================================
-
-	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
-	// and another one with "jpinkman" then completes successfully.
-	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
-	}
-
-//========================================================================================
-
-	// TODO Expect 10 elements then complete and notice how long the test takes.
-	void expect10Elements(Flux<Long> flux) {
-		fail();
-	}
+    
+    // TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException
+    //  error.
+    void expectFooBarError(Flux<String> flux) {
+        StepVerifier.create(flux)
+                .expectNext("foo",
+                            "bar")
+                .verifyError(RuntimeException.class);
+    }
 
 //========================================================================================
+    
+    // TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
+    // and another one with "jpinkman" then completes successfully.
+    void expectSkylerJesseComplete(Flux<User> flux) {
+        StepVerifier.create(flux)
+                .expectNextMatches(user -> user.getUsername()
+                        .equals("swhite"))
+                .expectNextMatches(user -> user.getUsername()
+                        .equals("jpinkman"))
+                .verifyComplete();
+        
+        StepVerifier.create(flux)
+                .assertNext(user -> Assertions.assertThat(user.getUsername())
+                        .isEqualTo("swhite"))
+                .assertNext(user -> Assertions.assertThat(user.getUsername())
+                        .isEqualTo("jpinkman"))
+                .verifyComplete();
 
-	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
-	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
-	void expect3600Elements(Supplier<Flux<Long>> supplier) {
-		fail();
-	}
+//        StepVerifier.create(flux)
+//                .expectNextMatches(user -> user.getUsername().equals("swhite"))
+//                .assertNext(user -> Assertions.assertThat(user.getUsername()).isEqualToIgnoringCase("jpinkman"))
+//                .verifyComplete(); // TO BE REMOVED
+    }
 
-	private void fail() {
-		throw new AssertionError("workshop not implemented");
-	}
+//========================================================================================
+    
+    // TODO Expect 10 elements then complete and notice how long the test takes.
+    void expect10Elements(Flux<Long> flux) {
+        StepVerifier.create(flux).expectNextCount(10).verifyComplete();
+    }
 
+//========================================================================================
+    
+    // TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
+    // by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
+    void expect3600Elements(Supplier<Flux<Long>> supplier) {
+        StepVerifier.withVirtualTime(supplier)
+                .thenAwait(Duration.ofHours(1))
+                .expectNextCount(3600)
+                .verifyComplete(); // TO BE REMOVED
+    }
+    
+    private void fail() {
+        throw new AssertionError("workshop not implemented");
+    }
+    
 }
