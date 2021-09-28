ac2879aaf32e77aff989bf1ca8f9ae232afe2a79
Anand Pardhi <mail@anand-pardhi.com>
2021-09-29 01:44:53 +1000

    My changes Flux, Mono and just methods

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -12,28 +12,45 @@ import reactor.core.publisher.Mono;
 public class Part04Transform {
 
 //========================================================================================
-
-	// TODO Capitalize the user username, firstname and lastname
-	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
-	}
+    
+    // TODO Capitalize the user username, firstname and lastname
+    Mono<User> capitalizeOne(Mono<User> mono) {
+        return mono.map(e -> {
+            return new User(e.getUsername()
+                                    .toUpperCase(),
+                            e.getFirstname()
+                                    .toUpperCase(),
+                            e.getLastname()
+                                    .toUpperCase());
+        });
+    }
 
 //========================================================================================
-
-	// TODO Capitalize the users username, firstName and lastName
-	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
-	}
+    
+    // TODO Capitalize the users username, firstName and lastName
+    Flux<User> capitalizeMany(Flux<User> flux) {
+        return flux.flatMap(e -> Flux.just(new User(e.getUsername()
+                                                            .toUpperCase(),
+                                                    e.getFirstname()
+                                                            .toUpperCase(),
+                                                    e.getLastname()
+                                                            .toUpperCase())));
+    }
 
 //========================================================================================
-
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
-	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
-	}
-
-	Mono<User> asyncCapitalizeUser(User u) {
-		return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
-	}
-
+    
+    // TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+    Flux<User> asyncCapitalizeMany(Flux<User> flux) {
+        return flux.flatMap ( u -> asyncCapitalizeUser(u));
+    }
+    
+    Mono<User> asyncCapitalizeUser(User u) {
+        return Mono.just(new User(u.getUsername()
+                                          .toUpperCase(),
+                                  u.getFirstname()
+                                          .toUpperCase(),
+                                  u.getLastname()
+                                          .toUpperCase()));
+    }
+    
 }
